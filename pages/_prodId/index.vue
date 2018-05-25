95f5131c05d0de2d5d7181a0387111d6cbb00372
Sergey Klitko <sergey.klitko@netzlabor-coding.de>
2018-05-25 14:48:56 +0300

    add _prodID

---
+++ pages/_prodId/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Header :transparentNavbar="transparentNavbar" />
+    <main class="section-content">
+      <div class="grid">
+        <component v-for="productItem in productItems" :key="productItem._uid" :blok="productItem" :is="productItem.component"></component>
+      </div>
+
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData(context) {
+    return context.app.$storyapi
+      .get('cdn/stories/products/' + context.params.id, {
+        dersion: 'draft'
+      })
+      .then(res => {
+        console.log(res.data)
+        return {
+          productItems: res.data.story.content.productItems,
+          transparentNavbar: res.data.story.content.transparent_navbar
+        }
+      })
+  }
+}
+</script>
